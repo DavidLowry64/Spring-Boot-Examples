f1dee3a698f929ca6fa9663f614c113c4ed64d18
David Lowry <DavidLowry64@gmail.com>
2021-10-05 16:28:12 -0400

    Initial Commit

---
+++ hello/src/main/java/com/dgl/springboot/HelloApplication.java
@@ -0,0 +1,15 @@
+package com.dgl.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+		
+		
+	}
+
+}
