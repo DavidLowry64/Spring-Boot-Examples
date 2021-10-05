5cbe677f63b822d3b52d5081c47064bc40a48b5f
David Lowry <DavidLowry64@gmail.com>
2021-10-05 19:25:33 -0400

    Initial Commit

---
+++ restServiceBasic/src/main/java/com/dgl/springboot/RestServiceBasicApplication.java
@@ -0,0 +1,13 @@
+package com.dgl.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceBasicApplication.class, args);
+	}
+
+}
---
+++ restServiceBasic/src/test/java/com/dgl/springboot/RestServiceBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgl.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
